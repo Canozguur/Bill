32dca6606f8af424e585299a504b25506ec60b43
Canozguur <59360738+Canozguur@users.noreply.github.com>
2021-01-01 22:35:05 +0300

    Create Bills.py

---
+++ Bills.py
@@ -0,0 +1,33 @@
+price_of_product_list = {"cigarette":20.0,
+                         "milk":5.0,
+                         "choclate":1.25}
+
+
+class Bill:
+    def __init__(self,name,surname ,bill):
+        self.name = name
+        self.surname = surname
+        self.bill = dict(bill)
+
+    def calculate(self):
+        print("Name :"," "*16,f"{str(self.name).upper()}")
+        print("Surname :", " "*13,f"{str(self.surname).upper()}")
+        all_price = 0
+        for product, num in zip(self.bill.keys(),self.bill.values()):
+            price = price_of_product_list.get(product)
+            price = price * num
+            all_price = price + all_price
+            num_of_char = len(product) + len(str(num))
+            print(f"Product Name : {product}({num}) ","-"*(20-num_of_char),f"{price}TL")
+        print(" "*45)
+        print("Total Price :","-"*25,f"{all_price}")
+        print("-"*45)
+
+
+
+
+
+user_stuff = {"cigarette":2,
+            "choclate":4}
+user = Bill("Can","Ozgur",user_stuff)
+user.calculate()
